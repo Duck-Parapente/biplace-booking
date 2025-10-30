0f6b5cb53306adbe2058cb8209576c1f37c340c3
jpaaax <jphilippe.ds@gmail.com>
2025-10-30 22:33:06 +0100

    add posthog

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -3,21 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     // Options
   },
   runtimeConfig: {
     public: {
-      auth0Domain: "biplace-duckparapente.eu.auth0.com",
-      auth0ClientId: process.env.NUXT_PUBLIC_AUTH0_CLIENT_ID
-    }
+      auth0Domain: 'biplace-duckparapente.eu.auth0.com',
+      auth0ClientId: process.env.NUXT_PUBLIC_AUTH0_CLIENT_ID,
+      posthogPublicKey: 'phc_wKnen6bP02l65gxk7g4o951oCvG5U5zJ4pRyz9HgVkT',
+      posthogHost: 'https://us.i.posthog.com',
+    },
   },
   app: {
-  head: {
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+    },
   },
-}
-})
+});
