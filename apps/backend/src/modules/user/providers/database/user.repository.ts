9df9ff268a443def1cba023ebd87dea585db7f77
jpaaax <jphilippe.ds@gmail.com>
2025-11-03 19:48:36 +0100

    remove publish from command

--- apps/backend/src/modules/user/providers/database/user.repository.ts
+++ apps/backend/src/modules/user/providers/database/user.repository.ts
@@ -1,11 +1,18 @@
 import { prisma } from '@libs/database/prisma/prisma';
+import { EventEmitterPort } from '@libs/events/domain/event-emitter.port';
 import { UserRepositoryPort } from '@modules/user/domain/ports/user.repository.port';
 import { UserEntity } from '@modules/user/domain/user.entity';
-import { Injectable, Logger } from '@nestjs/common';
+import { EVENT_EMITTER } from '@modules/user/user.di-tokens';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class UserRepository implements UserRepositoryPort {
   private readonly logger = new Logger(UserRepository.name);
+
+  constructor(
+    @Inject(EVENT_EMITTER)
+    private readonly eventEmitter: EventEmitterPort,
+  ) {}
 
   async save(user: UserEntity): Promise<void> {
     await prisma.user.create({
@@ -15,6 +22,8 @@ export class UserRepository implements UserRepositoryPort {
         externalAuthId: user.externalAuthId,
       },
     });
+
+    await user.publishEvents(this.eventEmitter);
     this.logger.log(`User saved: ${user.id} (${user.email})`);
   }
 }
