7798a93c47c85f8c46ec4045d93f0d7d8a3a3e32
jpaaax <jphilippe.ds@gmail.com>
2025-11-03 21:06:33 +0100

    handle cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,6 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
   app.use(json({ limit: '10mb' }));
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'x-api-key'],
+  });
+
   await app.listen(parseInt(process.env.PORT || '3001', 10));
   console.log('Backend started on', process.env.PORT || 3001);
 }
