0d5ff8efde9e90dcbc3e4e23f4b5fd92c81c4a80
jpaaax <jphilippe.ds@gmail.com>
2025-11-14 21:38:16 +0100

    remove log serializer

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -13,17 +13,6 @@ import { UserModule } from './modules/user/user.module';
     LoggerModule.forRoot({
       pinoHttp: {
         useLevel: 'info',
-        serializers: {
-          req: (req) => ({
-            method: req.method,
-            url: req.url,
-            params: req.params,
-            query: req.query,
-          }),
-          res: (res) => ({
-            statusCode: res.statusCode,
-          }),
-        },
       },
     }),
     UserModule,
