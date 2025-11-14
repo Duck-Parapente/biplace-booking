6ff057a56fb2c78b9d7a5a3123216c6603f2fd2e
jpaaax <jphilippe.ds@gmail.com>
2025-11-14 22:04:37 +0100

    fix lint staged for backend

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
 module.exports = {
   'apps/**/*.{js,jsx,ts,tsx,vue}': ['eslint --cache --fix', 'prettier --write'],
-  'apps/backend/**/*.ts': () => 'cd apps/backend && pnpm lint:deps',
+  'apps/backend/**/*.ts': ['pnpm --filter=backend lint:deps'],
 };
