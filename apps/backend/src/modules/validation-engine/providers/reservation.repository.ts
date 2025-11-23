5866b818016131f2437f14af5b365c93e9db1396
jpaaax <jphilippe.ds@gmail.com>
2025-11-23 22:35:12 +0100

    create reservation repo

---
+++ apps/backend/src/modules/validation-engine/providers/reservation.repository.ts
@@ -0,0 +1,36 @@
+import { DateValueObject } from '@libs/ddd/date.value-object';
+import { UUID } from '@libs/ddd/uuid.value-object';
+import { Injectable, Logger } from '@nestjs/common';
+
+import { ReservationRepositoryPort } from '../domain/ports/reservation.repository.port';
+import { ReservationWishSummary } from '../domain/validation-engine.types';
+
+@Injectable()
+export class ReservationRepository implements ReservationRepositoryPort {
+  private readonly logger = new Logger(ReservationRepository.name);
+
+  constructor() {}
+
+  findByPackAndDate(
+    packId: UUID,
+    startingDate: DateValueObject,
+    endingDate: DateValueObject,
+  ): Promise<ReservationWishSummary | null> {
+    this.logger.log(
+      `Finding reservation for pack ${packId.uuid} between ${startingDate.toString()} and ${endingDate.toString()}`,
+    );
+    throw new Error('Method not implemented.');
+  }
+
+  create(props: {
+    packId: UUID;
+    userId: UUID;
+    startingDate: DateValueObject;
+    endingDate: DateValueObject;
+    reservationWishId: UUID;
+    publicComment: string;
+  }): Promise<void> {
+    this.logger.log(`Creating reservation for pack ${props.packId.uuid}`);
+    throw new Error('Method not implemented.');
+  }
+}
--- apps/backend/src/modules/validation-engine/validation-engine.module.ts
+++ apps/backend/src/modules/validation-engine/validation-engine.module.ts
@@ -5,9 +5,14 @@ import { Module } from '@nestjs/common';
 import { AttibutePackCliController } from './commands/attribute-packs.cli.controller';
 import { AttributionDomainService } from './domain/attibution.domain-service';
 import { AttributePacksDomainService } from './domain/attribute-packs.domain-service';
-import { PackRepository } from './providers/pack.repository.port';
-import { ReservationWishRepository } from './providers/reservation-wish.repository.port';
-import { PACK_REPOSITORY, RESERVATION_WISH_REPOSITORY } from './validation-engine.di-tokens';
+import { PackRepository } from './providers/pack.repository';
+import { ReservationWishRepository } from './providers/reservation-wish.repository';
+import { ReservationRepository } from './providers/reservation.repository';
+import {
+  PACK_REPOSITORY,
+  RESERVATION_REPOSITORY,
+  RESERVATION_WISH_REPOSITORY,
+} from './validation-engine.di-tokens';
 
 @Module({
   imports: [],
@@ -15,6 +20,7 @@ import { PACK_REPOSITORY, RESERVATION_WISH_REPOSITORY } from './validation-engin
   providers: [
     { provide: PACK_REPOSITORY, useClass: PackRepository },
     { provide: RESERVATION_WISH_REPOSITORY, useClass: ReservationWishRepository },
+    { provide: RESERVATION_REPOSITORY, useClass: ReservationRepository },
     AttibutePackCliController,
     AttributionDomainService,
     AttributePacksDomainService,
