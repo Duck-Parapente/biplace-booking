ac365722be965a30f84c071aed8f949f59b39f68
jpaaax <jphilippe.ds@gmail.com>
2025-12-10 21:50:03 +0100

    remove private comment

---
+++ apps/backend/src/libs/database/prisma/migrations/20251210203552_drop_private_comment/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `privateComment` on the `FlightLog` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FlightLog" DROP COLUMN "privateComment";
