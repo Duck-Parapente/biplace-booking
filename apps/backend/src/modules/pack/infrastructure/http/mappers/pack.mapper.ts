2d259076c03e1aa4eeed86d44f7c2b3a21c49ebb
jpaaax <jphilippe.ds@gmail.com>
2026-02-20 17:44:28 +0100

    fix date in packs dto

--- apps/backend/src/modules/pack/infrastructure/http/mappers/pack.mapper.ts
+++ apps/backend/src/modules/pack/infrastructure/http/mappers/pack.mapper.ts
@@ -1,4 +1,5 @@
 import { PackEntity } from '@modules/pack/domain/pack.entity';
+import { format } from 'date-fns';
 import { PackDto } from 'shared';
 
 export function mapPackToDto(pack: PackEntity): PackDto {
@@ -10,8 +11,10 @@ export function mapPackToDto(pack: PackEntity): PackDto {
     flightsCount: pack.flightsCount,
     description: pack.description,
     details: pack.details,
-    lastControlDate: pack.lastControlDate?.value.toISOString() ?? null,
-    lastRescueFoldingDate: pack.lastRescueFoldingDate?.value.toISOString() ?? null,
+    lastControlDate: pack.lastControlDate ? format(pack.lastControlDate.value, 'yyyy-MM-dd') : null,
+    lastRescueFoldingDate: pack.lastRescueFoldingDate
+      ? format(pack.lastRescueFoldingDate.value, 'yyyy-MM-dd')
+      : null,
     order: pack.order,
   };
 }
