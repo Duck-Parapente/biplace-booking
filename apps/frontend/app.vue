ce518fad1ca97b5988e4e44d4b042b4fcc3a31cc
jpaaax <jphilippe.ds@gmail.com>
2026-02-20 19:06:08 +0100

    fix base root overflow

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex flex-col">
+  <div class="h-screen flex flex-col overflow-x-hidden">
     <AppHeader class="flex-shrink-0" />
     <NuxtPage class="flex-1 overflow-y-auto" />
   </div>
