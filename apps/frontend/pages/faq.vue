783eb2307106ba941328e6bd2d1c3bb2c39bfbc6
Julien Devillers <qdaemon.fr@gmail.com>
2026-03-04 00:12:09 +0100

    remplacement de charte par convetion de réservation qui est le titre du document.

--- apps/frontend/pages/faq.vue
+++ apps/frontend/pages/faq.vue
@@ -59,7 +59,7 @@
             <li>
               Avoir LU ET signé et envoyé la
               <a href="https://drive.google.com/file/d/1GSdB7lp_31N4jSX0m7lzxjC3m1EXwyn0/view">
-                charte</a
+                convention de réservation</a
               >
               au président du Duck.
             </li>
