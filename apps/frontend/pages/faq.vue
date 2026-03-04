d5d650e99fe2fb9793dc1c60e06f6932141da286
Julien Devillers <qdaemon.fr@gmail.com>
2026-03-04 09:14:17 +0100

    Fix faq

--- apps/frontend/pages/faq.vue
+++ apps/frontend/pages/faq.vue
@@ -900,8 +900,12 @@
         <FaqItem
           question="11. J'ai une réservation, chez qui dois-je récupérer le matériel ?"
           v-show="isItemVisible(10)"
-        >
-          <p class="mb-3">Tu trouveras cette information avec le menu planning. Bon vol !</p>
+          ><p hidden>chercher</p>
+          <p class="mb-3">
+            Cette information se trouve sous le menu "Infos packs". Choisis ton pack et tu
+            obtiendras les caractérisiques du pack ainsi que son carnet de vols/réservations dans
+            lequel tu trouveras ce que tu cherches. Bon vol !
+          </p>
         </FaqItem>
 
         <!-- Stratégies et conseils -->
