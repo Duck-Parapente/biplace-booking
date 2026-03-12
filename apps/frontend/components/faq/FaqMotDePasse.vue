48be403d9d8e509f7c67cb3cb82be9ca80c96b62
Julien Devillers <qdaemon.fr@gmail.com>
2026-03-12 14:45:40 +0100

    Ajout d'une question dont la réponse pointe sur la réponse à une autre question.

--- apps/frontend/components/faq/FaqMotDePasse.vue
+++ apps/frontend/components/faq/FaqMotDePasse.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="mb-3">
-    Depuis la page d'accueil, clique sur bouton "Se connecter" puis sur "mot de passe oublié ? ".
-    Renseigne ton email et suis la procédure.
+    Depuis la <a href="https://biplace.duckparapente.fr">page d'accueil</a>, clique sur bouton "Se
+    connecter" puis sur "mot de passe oublié ? ". Renseigne ton email et suis la procédure.
   </p>
 </template>
